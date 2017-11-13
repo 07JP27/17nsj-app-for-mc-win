d3c2bc653e8569be1751e92a6fd2eee3e1746be5
07JP27 <jynpei.t@gmail.com>
2017-11-13 18:06:09 +0900

    bool反転コンバーターの実装

---
+++ 17nsj.app.mc.win/Converter/BoolReverseConverter.cs
@@ -0,0 +1,47 @@
+﻿//----------------------------------------------------------------------
+// <copyright file="BoolReverseConverter.cs" company="17NSJ PR Dept">
+// Copyright (c) 17NSJ PR Dept. All rights reserved.
+// </copyright>
+// <summary>BoolReverseConverterタクラス</summary>
+//----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17nsj.app.mc.win.Converter
+{
+    /// <summary>
+    /// BoolReverseConverterタクラス
+    /// </summary>
+    public class BoolReverseConverter
+    {
+        /// <summary>
+        /// 値を変換します。
+        /// </summary>
+        /// <param name="value">バインディング ソースによって生成された値</param>
+        /// <param name="targetType">バインディング ターゲット プロパティの型</param>
+        /// <param name="parameter">使用するコンバーター パラメーター</param>
+        /// <param name="culture">コンバーターで使用するカルチャ</param>
+        /// <returns>変換された値。メソッドが null を返す場合は、有効な null 値が使用されています。</returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(value is bool && (bool)value);
+        }
+
+        /// <summary>
+        /// 値を変換します。
+        /// </summary>
+        /// <param name="value">バインディング ターゲットによって生成される値</param>
+        /// <param name="targetType">変換後の型</param>
+        /// <param name="parameter">使用するコンバーター パラメーター</param>
+        /// <param name="culture">コンバーターで使用するカルチャ</param>
+        /// <returns>変換された値。メソッドが null を返す場合は、有効な null 値が使用されています。</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !(value is bool && (bool)value);
+        }
+    }
+}
